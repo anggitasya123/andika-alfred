aab0dfe2e273a6e5e8ff23e6ac1312ae21499390
anggitasya123 <angginiaagatha@gmail.com>
2026-02-13 08:36:06 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,8 @@
 import streamlit as st
 
+
 st.title("menghitung :blue[volume tabung] :sunglasses:")
 
-
+r = st.number_input("masukan jari-jari (cm) : ",0)
+t = st.number_input("masukan tinggi (cm) : ",0)
 
